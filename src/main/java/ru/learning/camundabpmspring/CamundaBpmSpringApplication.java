484950e53e99c35dbb1fe5d4860622140955f106
vgoncharov <goncharov.saratov@gmail.com>
2020-04-24 11:01:06 +0400

    first commint

---
+++ src/main/java/ru/learning/camundabpmspring/CamundaBpmSpringApplication.java
@@ -0,0 +1,13 @@
+package ru.learning.camundabpmspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaBpmSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamundaBpmSpringApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ru/learning/camundabpmspring/config/CamundaConfig.java
@@ -0,0 +1,39 @@
+package ru.learning.camundabpmspring.config;
+
+import org.camunda.bpm.engine.spring.SpringProcessEngineServicesConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+//@Import(SpringProcessEngineServicesConfiguration.class )
+public class CamundaConfig {
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource driver = new DriverManagerDataSource();
+        driver.setDriverClassName("org.postgresql.Driver");
+        driver.setUrl("jdbc:postgresql://localhost:5432/camundo");
+        driver.setUsername("admin");
+        driver.setPassword("admin");
+        return driver;
+    }
+ //   @Autowired
+//    private ResourcePatternResolver resourceLoader;
+
+/*
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        return new DataSourceTransactionManager(getPostgresDataSource());
+    }
+*/
+}
